15d59a129ce69e6212ddb1cba3462f1064f45c1c
moravit <moravit@post.bgo.ac.il>
2020-01-01 17:24:12 +0200

    adding split to train and test

--- skmultiflow/tests/__init__.py
+++ skmultiflow/tests/__init__.py
@@ -1,15 +1,20 @@
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 from skmultiflow import IfnClassifier
-from skmultiflow import _csvConveter
 
 clf = IfnClassifier(0.999)
 
-#data = _csvConveter.CsvConverter.convert("Chess.csv")
+file_path = "Chess.csv"
 
-df = pd.read_csv("Chess.csv")
+df = pd.read_csv(file_path)
 y = df['Class'].values
 X = df.drop(['Class'], axis = 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+clf.fit(X_train, y_train)
 
 X_without_cols_name = []
 for index, row in df.iterrows():
@@ -17,16 +22,21 @@ for index, row in df.iterrows():
     record = [elem for elem in row]
     X_without_cols_name.append(record)
 
-clf.fit(X, y)
 clf.add_training_set_error_rate(X_without_cols_name, y)
+
+
 clf.network.create_network_structure_file()
 
 
-z = _csvConveter.CsvConverter.convert_predict("Chess_test.csv")
+#z = _csvConveter.CsvConverter.convert_predict("Chess_test.csv")
 # -------------- predict will return the classes and write it to file --------------
 
-# print(clf.predict(z)
+y_pred = clf.predict(X_test)
+
+#print(y_pred)
+
+print(accuracy_score(y_test, y_pred))
 
 # -------------- predict_proba will return the probability for every class and write it to file --------------
-print(clf.predict_proba(z))
+print(clf.predict_proba(X_test))
 
