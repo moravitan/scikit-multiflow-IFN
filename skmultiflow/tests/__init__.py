83fa2d2a204b108f9fdd68a280dc98b5fd88e4b6
moravit <moravit@post.bgo.ac.il>
2020-01-01 16:22:33 +0200

    changing fit function signature in _ifnClassifier

--- skmultiflow/tests/__init__.py
+++ skmultiflow/tests/__init__.py
@@ -1,14 +1,24 @@
+import pandas as pd
+
 from skmultiflow import IfnClassifier
 from skmultiflow import _csvConveter
 
 clf = IfnClassifier(0.999)
-x = []
 
+#data = _csvConveter.CsvConverter.convert("Chess.csv")
 
-data = _csvConveter.CsvConverter.convert("Chess.csv")
+df = pd.read_csv("Chess.csv")
+y = df['Class'].values
+X = df.drop(['Class'], axis = 1)
 
-clf.fit(data[0], data[1], data[2])
-clf.add_training_set_error_rate(data[0], data[1])
+X_without_cols_name = []
+for index, row in df.iterrows():
+    # insert each sample in df to x
+    record = [elem for elem in row]
+    X_without_cols_name.append(record)
+
+clf.fit(X, y)
+clf.add_training_set_error_rate(X_without_cols_name, y)
 clf.network.create_network_structure_file()
 
 
