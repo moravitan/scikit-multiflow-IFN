61b56b639e19723982c893fe310d804840bce5fc
moravit <moravit@post.bgo.ac.il>
2020-01-01 21:40:13 +0200

    adding _dataProcessing class

--- skmultiflow/tests/__init__.py
+++ skmultiflow/tests/__init__.py
@@ -4,45 +4,35 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 from skmultiflow import IfnClassifier
+from skmultiflow._dataProcessing import DataProcessor
 
-clf = IfnClassifier(0.999)
+clf = IfnClassifier(0.95)
 
-file_path = "Chess.csv"
+file_path = "dataset.csv"
 
-df = pd.read_csv(file_path)
-y = df['Class'].values
-X = df.drop(['Class'], axis = 1)
+# df = pd.read_csv(file_path)
+# y = df['Class'].values
+# X = df.drop(['Class'], axis = 1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+dp = DataProcessor()
+
+X_train, X_test, y_train, y_test = dp.convert(file_path, 0.3)
 
 clf.fit(X_train, y_train)
 
 X_without_cols_name = []
-for index, row in df.iterrows():
+for index, row in X_train.iterrows():
     # insert each sample in df to x
     record = [elem for elem in row]
     X_without_cols_name.append(record)
 
-clf.add_training_set_error_rate(X_without_cols_name, y)
-
-
-
-#data = _csvConveter.CsvConverter.convert("Chess.csv")
-
-df = pd.read_csv("Chess.csv")
-y = df['Class'].values
-X = df.drop(['Class'], axis = 1)
-
-X_without_cols_name = []
-for index, row in df.iterrows():
-    # insert each sample in df to x
-    record = [elem for elem in row]
-    X_without_cols_name.append(record)
-
-clf.fit(X, y)
-clf.add_training_set_error_rate(X_without_cols_name, y)
+clf.add_training_set_error_rate(X_without_cols_name, y_train)
 
 clf.network.create_network_structure_file()
+
+
 
 
 #z = _csvConveter.CsvConverter.convert_predict("Chess_test.csv")
