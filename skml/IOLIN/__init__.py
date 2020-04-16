eb8cdc1efb0324ff0c2a90cf6e595798087b5dcc
moravit <moravit@post.bgo.ac.il>
2020-04-16 10:54:25 +0300

    refactor BasicIncremental class and adding abstrach class by the name OnlineNetwork

--- skml/IOLIN/__init__.py
+++ skml/IOLIN/__init__.py
@@ -3,6 +3,7 @@
 from ._Meta_Learning import MetaLearning
 from ._Regenerative import OnlineNetworkRegenerative
 from ._Basic_Incremental import BasicIncremental
+from ._OLIN import OnlineNetwork
 
 
-__all__ = ['MetaLearning', 'OnlineNetworkRegenerative', 'BasicIncremental']
+__all__ = ['MetaLearning', 'OnlineNetworkRegenerative', 'BasicIncremental', 'OnlineNetwork']
